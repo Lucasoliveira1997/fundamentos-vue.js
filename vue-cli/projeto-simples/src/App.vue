5a627f27d6d21e9667a92aa26c429781c9d300ba
Lucasoliveira1997 <lsdoliveira97@gmail.com>
2020-03-25 21:35:52 -0300

    vue cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <h1>{{ titulo }}</h1>
+        <button @click="alteraTexto()">Altera Texto</button>
+    </div>
+</template>
+
+<script>
+export default {
+    el: '#app',
+    data: function(){
+        return {
+            titulo: 'Ufa, Vue JS!'
+        }
+    },
+    methods: {
+        alteraTexto() {
+            return this.titulo += '#'
+        }
+    }
+}
+</script>
+
+<style>
+    #app {
+        background-color: bisque;
+        color: black;
+        font-size: 1.5em;
+    }
+</style>
